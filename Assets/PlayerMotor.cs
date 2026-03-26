6e503839d9d223217abbe3102fcab2d0e9c6b92d
dizzyizziewizzie <dsw61072@student.dsw.edu.pl>
2026-03-26 09:42:00 +0100

    player movement

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -4,20 +4,72 @@ using UnityEngine.InputSystem;
 public class PlayerMotor : MonoBehaviour
 {
     Vector2 direction;
-    public float speed = 10;
+    private bool canJump = true;
+    private Rigidbody2D rigidbody2D;
+    public float speed = 5;
+    public float jumpforce = 5;
+    public float maxspeed = 10;
+    public float stoppingforce = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        MovePlayer();
+
+        HandleMaxSpeed();
+
+        PlayerStopping();
+    }
+
+    private void MovePlayer()
+    {
+        rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
+    }
+
+    private void HandleMaxSpeed()
+    {
+        if (rigidbody2D.linearVelocityX >= maxspeed)
+        {
+            rigidbody2D.linearVelocityX = maxspeed;
+        }
+        else if (rigidbody2D.linearVelocityX <= -maxspeed)
+        {
+            rigidbody2D.linearVelocityX = -maxspeed;
+        }
+    }
+
+    private void PlayerStopping()
+    {
+        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingforce, 0));
+        }
     }
 
     private void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
     }
+
+
+    private void OnJump()
+    {
+        if (canJump)
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+
 }
+
+
